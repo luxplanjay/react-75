89f6bd85e0d0576263c3a933ab9b60b2d27bf07a
Alexander Repeta <alexander.repeta@gmail.com>
2023-07-20 23:43:10 +0300

    add examples

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import { useSelector } from 'react-redux';
-import { Account } from './Account';
-import { LangSwitcher } from './LangSwitcher';
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTasks } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
 
 export const App = () => {
-  const lang = useSelector(state => state.locale.lang);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
-    <div>
-      <LangSwitcher />
-      <hr />
-      <div>Current lang: {lang} </div>
-      <hr />
-      <Account />
-    </div>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <TaskList />
+    </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { App } from 'components/App';
-import './index.css';
-import { store } from 'redux/store';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { App } from "components/App";
+import { store } from "./redux/store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
